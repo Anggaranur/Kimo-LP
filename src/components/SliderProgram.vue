93b85c53a0c610bb0f7d8e45e97b09c015896705
Anggaranur <m.anggaranurakbar@gmail.com>
2023-12-07 08:01:50 +0700

    update

--- src/components/SliderProgram.vue
+++ src/components/SliderProgram.vue
@@ -10,12 +10,11 @@
     >
       <vueper-slide v-for="(card, index) in cards" :key="index" :title="index.toString()">
         <template #content>
-          <!-- Box -->
           <div
             :class="['h-full w-[319px] mx-auto p-4 rounded-lg hover:shadow-xl duration-500', card.background, 'transition-transform transform hover:scale-90 relative ' ]"
           >
             <!-- Content -->
-            <img class="absolute " :src="card.image" alt="Card Image"/>
+            <img :src="card.image" alt="Card image" class="w-full h-auto" />
             <p class="text-center">{{ card.title }}</p>
             <p class="text-center">{{ card.content }}</p>
           </div>
@@ -34,14 +33,14 @@ export default {
   data() {
     return {
       cards: [
-        { title: 'Card 1', content: 'Content for Card 1', background: 'bg-red-400', image: '@/assets/program1.png'},
-        { title: 'Card 2', content: 'Content for Card 2', background: 'bg-blue-500', image: 'path/to/image2.jpg' },
-        { title: 'Card 3', content: 'Content for Card 3', background: 'bg-green-500', image: 'path/to/image3.jpg' },
-        { title: 'Card 4', content: 'Content for Card 4', background: 'bg-purple-500', image: 'path/to/image4.jpg' },
-        { title: 'Card 5', content: 'Content for Card 5', background: 'bg-pink-500', image: 'path/to/image5.jpg' },
-        { title: 'Card 6', content: 'Content for Card 6', background: 'bg-orange-500', image: 'path/to/image6.jpg' },
-        { title: 'Card 7', content: 'Content for Card 7', background: 'bg-yellow-500', image: 'path/to/image7.jpg' },
-        { title: 'Card 8', content: 'Content for Card 8', background: 'bg-blue-900', image: 'path/to/image8.jpg' },
+        { title: 'Card 1', content: 'Content for Card 1', background: 'bg-red-400', image: 'src/assets/logo-tiktok.svg' },
+        { title: 'Card 2', content: 'Content for Card 2', background: 'bg-blue-500', image: './assets/card2.jpg' },
+        { title: 'Card 3', content: 'Content for Card 3', background: 'bg-green-500', image: './assets/card3.jpg' },
+        { title: 'Card 4', content: 'Content for Card 4', background: 'bg-purple-500', image: './assets/card4.jpg' },
+        { title: 'Card 5', content: 'Content for Card 5', background: 'bg-pink-500', image: './assets/card5.jpg' },
+        { title: 'Card 6', content: 'Content for Card 6', background: 'bg-orange-500', image: './assets/card6.jpg' },
+        { title: 'Card 7', content: 'Content for Card 7', background: 'bg-yellow-500', image: './assets/card7.jpg' },
+        { title: 'Card 8', content: 'Content for Card 8', background: 'bg-blue-900', image: './assets/card8.jpg' },
       ],
     };
   },
