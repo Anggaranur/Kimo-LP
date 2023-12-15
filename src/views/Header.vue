0825844ac76cca7028fd201e43952ef7014b3393
Anggaranur <m.anggaranurakbar@gmail.com>
2023-12-15 09:51:05 +0700

    update

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -1,27 +1,29 @@
 <template>
- <header class="bg-default h-[708px] max-xl:w-[1500px]  relative">
+ <header class="bg-default h-[708px] max-xl:w-[1500px] max-lg:h-[900px] relative">
     <!-- NavBar -->
-    <nav class="bg-white h-[77px] fixed w-full z-50 shadow-xl max-xl:h-[150px]">
-      <img class="h-[77px] w-[212px]" src="@/assets/logo-kimo-nav.png"/>
+    <nav class="bg-white h-[77px] fixed w-full z-50 shadow-xl max-lg:h-[180px]">
+      <img class="h-[77px] w-[212px] absolute max-lg:w-[390px] max-lg:h-[140px] max-lg:top-5" src="@/assets/logo-kimo-nav.png"/>
       <!-- Button Dowmload -->
       <a href="#download" v-smooth-scroll="{ duration: 1000, offset: -50 }">
         <button class="absolute top-3.5 right-5 bg-redButton p-3 text-white font-Roboto 
-        font-bold rounded-md hover:shadow-md duration-300 active:opacity-60 transition-transform transform hover:scale-110" title="Download Apps">
+        font-bold rounded-md hover:shadow-md duration-300 active:opacity-60 transition-transform transform hover:scale-110
+        max-lg:p-10 max-lg:text-2xl max-lg:top-8" title="Download Apps">
           DOWNLOAD SEKARANG!
         </button>
       </a>
     </nav>
 
     <!-- Header Content -->
-    <div class="bg-white absolute top-[120px] right-12 left-12 h-[546px] rounded-xl hover:shadow-2xl duration-300">
+    <div class="bg-white absolute top-[120px] right-12 left-12 h-[546px] rounded-xl hover:shadow-2xl duration-300 max-lg:top-[200px] max-lg:h-[670px]">
       <!-- Circle Animation -->
       <CircleMove />
-      <h1 class="absolute z-30 max-w-[780px] h-[162px] font-Roboto font-[700] text-[40px] top-44 left-10 pointer-events-none">
+      <h1 class="absolute z-30 max-w-[780px] h-[162px] font-Roboto font-[700] text-[40px] top-44 left-10 pointer-events-none max-lg:text-[55px]">
         Selamat datang di Kimo, pangkalan rahasia bagi para pemberani yang siap menjelajahi dunia bahasa Inggris! 🌍✨
       </h1>
 
       <!-- menu page scroll bounce -->
-      <div class="absolute bg-default top-[530px] h-[65px] w-[65px] rounded-full left-[700px] text-white animate-bounce shadow-2xl z-40 cursor-pointer" title="See News">
+      <div class="absolute bg-default top-[530px] h-[65px] w-[65px] rounded-full left-[700px] text-white animate-bounce shadow-2xl z-40 cursor-pointer
+      max-lg:h-[110px] max-lg:w-[110px] max-lg:top-[600px]" title="See News">
         <a href="#news" v-smooth-scroll="{ duration: 1000, offset: -50 }">
           <img src="@/assets/down-arrow.png">
         </a>
