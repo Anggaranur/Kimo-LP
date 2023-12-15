509ebb2e63ff29c75d0a638a5f2cf72d16933d42
Anggaranur <m.anggaranurakbar@gmail.com>
2023-12-15 10:24:02 +0700

    update

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -16,22 +16,22 @@
     <!-- Header Content -->
     <div class="bg-white absolute top-[120px] right-12 left-12 h-[546px] rounded-xl hover:shadow-2xl duration-300 max-md:top-[400px] max-md:h-[2305px]">
       <!-- Circle Animation -->
-      <CircleMove />
-      <h1 class="absolute z-30 max-w-[780px] h-[162px] font-Roboto font-[700] text-[40px] top-44 left-10 pointer-events-none max-md:text-[55px]">
+      <CircleMove class="absolute max-md:top-[1300px] max-md:left-[100px]" />
+      <h1 class="absolute z-30 max-w-[780px] h-[162px] font-Roboto font-[700] text-[40px] top-44 left-10 pointer-events-none max-md:text-[55px] max-md:top-[1500px]">
         Selamat datang di Kimo, pangkalan rahasia bagi para pemberani yang siap menjelajahi dunia bahasa Inggris! 🌍✨
       </h1>
 
       <!-- menu page scroll bounce -->
       <div class="absolute bg-default top-[530px] h-[65px] w-[65px] rounded-full left-[700px] text-white animate-bounce shadow-2xl z-40 cursor-pointer
-      max-lg:h-[110px] max-lg:w-[110px] max-lg:top-[600px]" title="See News">
+      max-md:h-[200px] max-md:w-[200px] max-md:top-[2200px] max-md:left-[615px]" title="See News">
         <a href="#news" v-smooth-scroll="{ duration: 1000, offset: -50 }">
           <img src="@/assets/down-arrow.png">
         </a>
       </div>
 
       <!-- Logo Header -->
-      <div class=" absolute top-0  right-3 w-[820px] h-[546px]">
-        <img class="absolute h-[546px] right-2" src="@/assets/kimo-header.png">
+      <div class=" absolute top-0  right-3 w-[820px]  max-md:w-[2400px]">
+        <img class="absolute h-[546px] right-2 max-md:h-[1200px] max-md:right-28" src="@/assets/kimo-header.png">
       </div>
     </div>
   </header>
